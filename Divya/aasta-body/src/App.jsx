95de19aff41c69c63acd50e1631345ea50790a7b
divyasonla <divyasonla23@navgurukul.org>
2025-07-12 17:19:50 +0530

    starting project add alumini page

---
+++ Divya/aasta-body/src/App.jsx
@@ -0,0 +1,14 @@
+import Alumini from "./pages/alumini"
+// import Students from "./pages/students"
+function App() {
+
+  return (
+    <>
+    <Alumini />
+    {/* <Students /> */}
+      {/* <h1 className="bg-red-200">hii, my name is divya</h1> */}
+    </>
+  )
+}
+
+export default App
